70fe5c22d6274a77245185f251ca25c537890b6b
Michaela Antoš <michaela.antos1@gmail.com>
2024-04-10 23:33:44 +0200

    treti ukol

---
+++ hra.js
@@ -0,0 +1,46 @@
+let currentPlayer = 'circle';
+
+const selectbutton = (evt) => {
+  const btn = evt.target;
+  evt.target.disabled = true;
+  if (currentPlayer === 'circle') {
+    btn.classList.add('board__field--circle');
+    document.querySelector('#current-player').src = 'pictures/circle.svg';
+    currentPlayer = 'cross';
+  } else {
+    btn.classList.add('board__field--cross');
+    document.querySelector('#current-player').src = 'pictures/cross.svg';
+    currentPlayer = 'circle';
+  }
+};
+
+document
+  .querySelector('button:nth-child(1)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(2)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(3)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(4)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(5)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(6)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(7)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(8)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(9)')
+  .addEventListener('click', selectbutton);
+document
+  .querySelector('button:nth-child(10)')
+  .addEventListener('click', selectbutton);
