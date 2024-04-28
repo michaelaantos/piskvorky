7f415a1d34c460210603ce72bd2b2f2ef132ac78
Michaela Antoš <michaela.antos1@gmail.com>
2024-04-28 22:04:31 +0200

    ukol c. 5

--- hra.js
+++ hra.js
@@ -1,26 +1,7 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
 let currentPlayer = 'circle';
-
-const selectbutton = (evt) => {
-  const btn = evt.target;
-  evt.target.disabled = true;
-  if (currentPlayer === 'circle') {
-    btn.classList.add('board__field--circle');
-    currentPlayer = 'cross';
-    document.querySelector('#current-player').src = 'pictures/cross.svg';
-  } else {
-    btn.classList.add('board__field--cross');
-    currentPlayer = 'circle';
-    document.querySelector('#current-player').src = 'pictures/circle.svg';
-  }
-  checkGameStatus();
-};
-
 const selectAll = document.querySelectorAll('.game__box--square');
-selectAll.forEach((button) => {
-  button.addEventListener('click', selectbutton);
-});
 
 const changeElm = (htmlButton) => {
   if (htmlButton.classList.contains('board__field--cross')) {
@@ -38,9 +19,7 @@ const checkGameStatus = () => {
 
   if (vitez === 'o' || vitez === 'x') {
     setTimeout(() => {
-      console.log('funguje');
       alert(`Vyhrál hráč se symbolem ${vitez}.`);
-
       location.reload();
     }, 1000);
   } else if (vitez === 'tie') {
@@ -48,5 +27,52 @@ const checkGameStatus = () => {
       alert('Hra skončila remízou');
       location.reload();
     }, 1000);
+  } else if (vitez === null && currentPlayer === 'cross') {
+    suggestNextMove();
   }
 };
+
+const suggestNextMove = async () => {
+  const herniPole = [...selectAll].map(changeElm);
+
+  const response = await fetch(
+    'https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        board: herniPole,
+        player: 'x',
+      }),
+    },
+  );
+
+  const data = await response.json();
+  const { x, y } = data.position;
+  const index = selectAll[x + y * 10];
+
+  index.click();
+};
+
+const selectButton = async (evt) => {
+  const btn = evt.target;
+  evt.target.disabled = true;
+
+  if (currentPlayer === 'circle') {
+    btn.classList.add('board__field--circle');
+    currentPlayer = 'cross';
+    document.querySelector('#current-player').src = 'pictures/cross.svg';
+  } else {
+    btn.classList.add('board__field--cross');
+    currentPlayer = 'circle';
+    document.querySelector('#current-player').src = 'pictures/circle.svg';
+  }
+
+  checkGameStatus();
+};
+
+selectAll.forEach((button) => {
+  button.addEventListener('click', selectButton);
+});
